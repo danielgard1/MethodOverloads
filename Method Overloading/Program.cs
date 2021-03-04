d9c524ad7f47f1916fa001411a391f30ee52e308
Daniel Gardner <danielgardner1357@gmail.com>
2021-03-03 19:38:01 -0700

    Exercise complete

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine(Add(2, 3, true));
+            Console.WriteLine(Add(1, 0, true));
+            Console.WriteLine(Add(10, 6, true));
+        }
+        static public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        static public string Add(int x, int y, bool z)
+        {
+            if (z == true && x + y != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else {
+                return $"{x + y} dollar";
+            }
+        }
+    }
+}
